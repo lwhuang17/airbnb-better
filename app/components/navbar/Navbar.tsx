caa7ca7480d8e73b603f8fc0edf91986097bb697
lwhuang17 <huang.liwei.17@gmail.com>
2023-10-06 21:39:00 -0700

    Add container, logo, search components
    - Add container (client) component
    - Add logo (server) component
    - Add search bar boilerplate

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Container from '../Container'
+import Logo from './Logo'
+import Search from './Search'
 
 const Navbar = () => {
     return (
@@ -9,7 +11,21 @@ const Navbar = () => {
                 border-b-[1px]
             "
             >
-                <Container></Container>
+                <Container>
+                    <div
+                        className="
+                            flex
+                            flex-row
+                            items-center
+                            justify-between
+                            gap-3
+                            md:gap-0
+                        "
+                    >
+                        <Logo />
+                        <Search />
+                    </div>
+                </Container>
             </div>
         </div>
     )
