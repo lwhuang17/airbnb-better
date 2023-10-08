17d54d81f1d5b5f87654cf818a083f3e3fafecbc
lwhuang17 <huang.liwei.17@gmail.com>
2023-10-07 23:50:10 -0700

    Add modal component with custom button component.
    - Reformat with prettier on some files
    - New button styles with primary and secondary styles.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,30 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Nunito } from 'next/font/google'
-import Navbar from './components/navbar/Navbar'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modal/Modal";
 
 export const metadata: Metadata = {
-  title: 'Airbnb Better',
-  description: 'The better version of Airbnb',
-}
+  title: "Airbnb Better",
+  description: "The better version of Airbnb",
+};
 
 const font = Nunito({
-  subsets: ["latin"]
+  subsets: ["latin"],
 });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
         <Navbar />
+        <Modal isOpen title="Hello World" actionLabel="Submit" />
         {children}
       </body>
     </html>
-  )
+  );
 }
