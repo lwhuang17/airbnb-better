ed6529f4b96bbb21de458093848475797a4bff45
lwhuang17 <huang.liwei.17@gmail.com>
2023-10-12 15:16:35 -0700

    Add categories on main page and connect it to individual url params.

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 import { SafeUser } from "@/app/types";
+import Categories from "./Categories";
 
 interface NavbarProps {
   currentUser?: SafeUser | null;
@@ -37,6 +38,7 @@ const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
           </div>
         </Container>
       </div>
+      <Categories />
     </div>
   );
 };
