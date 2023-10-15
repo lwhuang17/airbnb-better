2b2d5a551bc521aade80dc590ab6aef0fc9b1b7f
lwhuang17 <huang.liwei.17@gmail.com>
2023-10-14 21:47:52 -0700

    Add location select on rent modal.

--- app/components/modal/RentModal.tsx
+++ app/components/modal/RentModal.tsx
@@ -6,6 +6,7 @@ import { useMemo, useState } from "react";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
+import CountrySelect from "../inputs/CountrySelect";
 
 enum STEPS {
   CATEGORY,
@@ -58,6 +59,7 @@ const RentModal = () => {
   });
 
   const category = watch("category");
+  const location = watch("location");
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
       shouldDirty: true,
@@ -66,7 +68,7 @@ const RentModal = () => {
     });
   };
 
-  const bodyContent = (
+  let bodyContent = (
     <div className="flex flex-col gap-8">
       <Heading
         title="Which of these best describe your place?"
@@ -96,6 +98,21 @@ const RentModal = () => {
       </div>
     </div>
   );
+
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Where is your place located?"
+          subtitle="Help guests find you!"
+        />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue("location", value)}
+        />
+      </div>
+    );
+  }
 
   return (
     <Modal
