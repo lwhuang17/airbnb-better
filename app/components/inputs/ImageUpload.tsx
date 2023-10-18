80b3bf319c3c8a22c0aa609f9beecec650929b16
lwhuang17 <huang.liwei.17@gmail.com>
2023-10-18 11:53:51 -0700

    Create image upload step and component.

---
+++ app/components/inputs/ImageUpload.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { CldUploadWidget, CldUploadWidgetResults } from "next-cloudinary";
+import Image from "next/image";
+import { useCallback } from "react";
+import { TbPhotoPlus } from "react-icons/tb";
+
+interface ImageUploadProps {
+  onChange: (value: string) => void;
+  imgSrc: string;
+}
+
+const ImageUpload: React.FC<ImageUploadProps> = ({ onChange, imgSrc }) => {
+  const handleUpload = useCallback(
+    (result: CldUploadWidgetResults) => {
+      onChange((result.info as { secure_url: string }).secure_url);
+    },
+    [onChange]
+  );
+
+  return (
+    <CldUploadWidget
+      onUpload={handleUpload}
+      uploadPreset="kif2k1qi"
+      options={{ maxFiles: 1 }}
+    >
+      {({ open }) => {
+        return (
+          <div
+            onClick={() => open?.()}
+            className="
+              relative
+              flex
+              cursor-pointer
+              flex-col
+              items-center
+              justify-center
+              gap-4
+              border-2
+              border-dashed
+              border-neutral-300
+              p-20
+              text-neutral-600
+              transition
+              hover:opacity-70
+            "
+          >
+            <TbPhotoPlus size={50} />
+            <div className="text-lg font-semibold"> Click to upload </div>
+            {imgSrc && (
+              <div className="absolute inset-0 h-full w-full">
+                <Image
+                  alt="Upload"
+                  fill
+                  style={{ objectFit: "cover" }}
+                  src={imgSrc}
+                />
+              </div>
+            )}
+          </div>
+        );
+      }}
+    </CldUploadWidget>
+  );
+};
+
+export default ImageUpload;
