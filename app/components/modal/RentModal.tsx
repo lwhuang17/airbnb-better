dcc2b4a9075941fde53fc76efeba37b3e0c49174
lwhuang17 <huang.liwei.17@gmail.com>
2023-10-18 22:45:08 -0700

    Add description and price steps for listing.

--- app/components/modal/RentModal.tsx
+++ app/components/modal/RentModal.tsx
@@ -10,6 +10,7 @@ import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
 import ImageUpload from "../inputs/ImageUpload";
+import Input from "../inputs/Input";
 
 enum STEPS {
   CATEGORY,
@@ -174,6 +175,54 @@ const RentModal = () => {
         <ImageUpload
           imgSrc={imgSrc}
           onChange={(src) => setCustomValue("imgSrc", src)}
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.DESCRIPTION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="How would you describe your place"
+          subtitle="Short and sweet works best!"
+        />
+        <Input
+          id="title"
+          label="Title"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <hr />
+        <Input
+          id="description"
+          label="Description"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.PRICE) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Now, set your price"
+          subtitle="How much do you charge per night?"
+        />
+        <Input
+          id="price"
+          label="Price"
+          register={register}
+          formatPrice
+          disabled={isLoading}
+          errors={errors}
+          required
         />
       </div>
     );
