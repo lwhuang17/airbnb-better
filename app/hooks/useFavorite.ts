a2011a6de62637078ae6535b6d952cf41432c65b
lwhuang17 <huang.liwei.17@gmail.com>
2023-10-22 19:04:40 -0700

    Add favorite and unfavorite rest api endpoints.

---
+++ app/hooks/useFavorite.ts
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import useLoginModal from "./useLoginModal";
+import { useRouter } from "next/navigation";
+import { SafeUser } from "../types";
+import { useCallback, useMemo } from "react";
+
+interface IUseFavorite {
+  listingId: string;
+  currentUser?: SafeUser | null;
+}
+const useFavorite = ({ listingId, currentUser }: IUseFavorite) => {
+  const router = useRouter();
+  const loginModal = useLoginModal();
+
+  const hasFavorited = useMemo(() => {
+    const list = currentUser?.favoriteIds || [];
+    return list.includes(listingId);
+  }, [currentUser, listingId]);
+
+  const toggleFavorite = useCallback(
+    async (e: React.MouseEvent<HTMLDivElement>) => {
+      e.stopPropagation();
+
+      if (!currentUser) {
+        return loginModal.onOpen();
+      }
+
+      try {
+        if (hasFavorited) {
+          await axios.delete(`/api/favorites/${listingId}`);
+          toast.success(`Removed from favorites.`);
+        } else {
+          await axios.post(`/api/favorites/${listingId}`);
+          toast.success(`Added to favorites.`);
+        }
+        router.refresh();
+      } catch (_) {
+        toast.error("Something went wrong.");
+      }
+    },
+    [currentUser, hasFavorited, router, loginModal, listingId]
+  );
+
+  return { hasFavorited, toggleFavorite };
+};
+
+export default useFavorite;
